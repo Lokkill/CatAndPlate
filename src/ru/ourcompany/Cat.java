1dccf7259b21c3b5aaaf735d01083b909574d15b
Андрей Катаев <crovolk@yandex.ru>
2020-10-19 08:27:51 +0300

    Completing homework

--- src/ru/ourcompany/Cat.java
+++ src/ru/ourcompany/Cat.java
@@ -1,4 +1,16 @@
 package ru.ourcompany;
 
 public class Cat {
+    protected String name;
+    protected int needFood;
+    protected boolean isFull;
+
+    Cat(String name, int needFood) {
+        this.name = name;
+        this.needFood = needFood;
+    }
+
+    public void eat(Plate plate) {
+        this.isFull = plate.eatFromAPlate(needFood, isFull);
+    }
 }
--- src/ru/ourcompany/Plate.java
+++ src/ru/ourcompany/Plate.java
@@ -1,4 +1,25 @@
 package ru.ourcompany;
 
 public class Plate {
+    protected static int foodIn;
+
+    Plate(int foodIn) {
+        Plate.foodIn = foodIn;
+    }
+
+    public boolean eatFromAPlate(int countFood, boolean isFull) {
+        if (isFull) return true; // Если кот сыт - он не будет есть
+        if (foodIn >= countFood) {
+            foodIn -= countFood;
+            return true;
+        } else {
+            System.out.println("В тарелке закончилась еда!");
+            return false;
+        }
+    }
+
+    public void addFood(int countFood) {
+        if (countFood > 0) foodIn += countFood;
+        System.out.println("В тарелку добавили " + countFood + " единиц еды. Теперь в ней: " + foodIn + " единиц еды.");
+    }
 }
